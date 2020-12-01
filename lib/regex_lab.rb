17fd1d4c76444f94c2e10d1603c756cc6957f2cd
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 04:00:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  return (word.match(/[aeiou]\w*/) != nil)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    return text.match(/[A-Z].*[!?.]/) != nil
 end
 
 def valid_phone_number?(phone)
