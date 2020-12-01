f0c4e21ee15df88791e397b6a68245df289d2e8f
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 04:20:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    return (text.match(/\A[A-Z].*[!?.]\z/) != nil)
+    return (text.match(/\A[0-9].*[!?.]\z/) != nil)
 end
