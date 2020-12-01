f0180a3ca336339ab45c809bbf1a080e6357f00e
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 04:07:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  return (text.scan(/\b[a-z]{5}\b/)
+  return (text.scan(/\b[a-z]{5}\b/))
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    return (text.match(/[A-Z].*[!?.]/)
+    return (text.match(/[A-Z].*[!?.]/))
 end
 
 def valid_phone_number?(phone)
