92064da98a1d070ebf0d60a6530fc6f8aea6e694
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 04:30:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    return (grep.match(/\A([0-9][()-]*){10}\z/) != nil)
+    return (grep.match(/\A(d[()-]*){10}\z/) != nil)
 end
